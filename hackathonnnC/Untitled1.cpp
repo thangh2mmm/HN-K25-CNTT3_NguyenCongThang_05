f86ca1a388cd0837bc9be156f94c13bea5a726ae
thangh2mmm <nguyencongthang08042000@gmail.com>
2025-11-06 09:52:40 +0700

    HN-K25-CNTT3_NguyenCongThang_03

---
+++ hackathonnnC/Untitled1.cpp
@@ -0,0 +1,155 @@
+#include<stdio.h>
+int main(){
+
+	
+	printf("==========================MENU============================\n");
+	printf("1.nhap so luong va gia tri tung phan tu cho mang\n");
+	printf("2.in ra cac gia tri phan tu trong mang\n");
+	printf("3.tinh tong cac phan tu chan va tong cac phan tu le trong mang\n");
+	printf("4.in ra gia tri lon nhat cua cac phan tu trong mang\n ");
+	printf("5.them moi 1 phan tu vao cuoi mang\n");
+	printf("6.xoa phan tu tai mot vi tri cu the(nguoi dung nhap)\n");
+	printf("7.sap xep mang theo thu tu tang dan(insertion sort)\n");
+	printf("8.kiem tra su ton tai cua phan tu trong mang\n");
+	printf("9.in ra cac phan tu xuat hien dung 2 lan trong mang\n");
+	printf("10.in ra so lan xuat hien cua mot phan tu\n");
+	
+
+
+	int choice=0;
+	int array[100];
+	int sizeMang;
+	printf("moi ban nhap vao lua chon cua minh:");
+	scanf("%d",&choice);
+	
+	
+	switch(choice){
+		case:1{
+		pritnf("nhap vao so luong phan tu cua mang:");
+		scanf("%d",&sizeMang);
+		
+		for(int i=0;i<=sizeMang;i++){
+			printf("phan tu thu %d:",i);
+			scanf("%d",&array[i]);
+			
+		}
+	
+	        break;
+		}
+	
+		case:2{
+			printf("cac gia tri phan tu trong mang la:")
+			for(int i=0;i<=sizeMang;i++){
+				printf("%d\n",array[i]);
+			}
+			break;
+				int sizeMang;
+		pritnf("nhap vao so luong phan tu cua mang:");
+		scanf("%d",&sizeMang);
+		
+		for(int i=0;i<=sizeMang;i++){
+			printf("phan tu thu %d:",i);
+			scanf("%d",&array[i]);
+			
+		}
+		
+		case:3{
+				int sizeMang;
+		pritnf("nhap vao so luong phan tu cua mang:");
+		scanf("%d",&sizeMang);
+		
+		for(int i=0;i<=sizeMang;i++){
+			printf("phan tu thu %d:",i);
+			scanf("%d",&array[i]);
+			
+			break;
+		}
+		
+		case:4{
+			pritnf("nhap vao so luong phan tu cua mang:");
+		scanf("%d",&sizeMang);
+		
+		for(int i=0;i<=sizeMang;i++){
+			printf("phan tu thu %d:",i);
+			scanf("%d",&array[i]);
+			pritnf("nhap vao so luong phan tu cua mang:");
+		scanf("%d",&sizeMang);
+		
+		for(int i=0;i<=sizeMang;i++){
+			printf("phan tu thu %d:",i);
+			scanf("%d",&array[i]);
+			
+		int flag=0;
+			for (int i=0;i<=sizeof(array)/sizeof(length);i++){
+				printf("phan tu thu %d:",i);
+				scanf("%d",&array[i]);
+				
+			}
+			
+			break;
+		}
+		
+		case:5{
+			pritnf("nhap vao so luong phan tu cua mang:");
+		scanf("%d",&sizeMang);
+		
+		for(int i=0;i<=sizeMang;i++){
+			printf("phan tu thu %d:",i);
+			scanf("%d",&array[i]);
+			for (int i=0;i<=sizeof(array)/sizeof(length);i++);
+			
+			
+			break;
+		}
+		
+		case:6{
+			printf("nhap vao so luong phan tu cua mang:");
+			scanf("%d",&array[i]);
+			
+			break;
+		}
+		
+		case:7{
+					pritnf("nhap vao so luong phan tu cua mang:");
+		scanf("%d",&sizeMang);
+		
+		for(int i=0;i<=sizeMang;i++){
+			printf("phan tu thu %d:",i);
+			scanf("%d",&array[i]);
+			
+			break;
+		
+		}
+		
+		case:8{
+			break;
+		}
+		
+		case:9{
+			break;
+		}
+		
+		case:10{
+			break;
+		}
+		
+		
+		default{
+			break;
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+
+	
+	return 0;
+}
+	
